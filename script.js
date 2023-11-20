c12fc0d2fbdfa82a7f22fceb101e3f265cef72d5
hsnpuply <hsnpuply@gmail.com>
2023-11-20 23:29:06 +0330

    ui page

---
+++ script.js
@@ -0,0 +1,12 @@
+const passwordbox = document.getElementById('password')
+const lenght = 12;
+
+const lowerCaseBox="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase =lowerCase.toLowerCase();
+const number = "0123456789"
+const symbol ="!@#$%^&*()_+|{}[]<?>/-=♥♣◘•▬ƒ₧¥ûá¢ÜÖ☺"
+
+
+function
