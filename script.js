4149d1ef61b7f275f0e8faaff2f67deb83be3d9d
hsnpuply <hsnpuply@gmail.com>
2023-11-21 00:26:16 +0330

    password genrate

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const passwordbox = document.getElementById('password')
 const lenght = 12;
 
-const lowerCaseBox="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase =lowerCase.toLowerCase();
+const lowerCase ='abcdefghijklmnopqrstuvwxyz';
 const number = "0123456789"
-const symbol ="!@#$%^&*()_+|{}[]<?>/-=♥♣◘•▬ƒ₧¥ûá¢ÜÖ☺"
+const symbol ="!@#$%^&*()_+|{}[]<?>/-=♥♣◘•▬ƒ₧¥ûá¢ÜÖ☺°±°¯²³´µ¶·¸"
 
+const allChars = upperCase + lowerCase + number + symbol;
 
-function
+function createPassword(){
+    let password='';
+
+    password +=upperCase[Math.floor(Math.random() * upperCase.length)];
+    password +=lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password +=number[Math.floor(Math.random() * number.length)];
+    password +=symbol[Math.floor(Math.random() * symbol.length)];
+    while(lenght > password.length){
+        password +=allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordbox.value=password;
+}
